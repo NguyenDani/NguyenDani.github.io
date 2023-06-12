c5cf49789c919f5ee5418a7277edf0e1e1dfbc3a
Daniel Nguyen <nguyendaniel1997@gmail.com>
2023-06-12 15:50:59 -0500

    Testing canvas

--- main.js
+++ main.js
@@ -1,74 +1,42 @@
-import './style.css'
+import './style.css';
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-/*
-// You will always have 3 objects Scene, Camera, Renderer
-// Scene is a container that holds all objects, cameras, lights, etc.
+
 const scene = new THREE.Scene();
 
-// Camera allows the user to see the scene
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-// Renderer draws the objects
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
-});
+})
 
 renderer.setPixelRatio(window.devicePixelRatio);
+// Set size of renderer
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
+const geometry = new THREE.TorusGeometry(10, 3 , 16, 100);
+
+const material = new THREE.MeshBasicMaterial({color: 0xff6347, wireframe: true});
 const torus = new THREE.Mesh(geometry, material);
 
-scene.add(torus)
+scene.add(torus);
 
-const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(5, 5, 5)
-
-const ambientLight = new THREE.AmbientLight(0xffffff)
-scene.add(pointLight, ambientLight)
-
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper)
-
-const controls = new OrbitControls(camera, renderer.domElement);
-
-function addStar(){
-    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-    const material = new THREE.MeshStandardMaterial({color: 0xffffff})
-    const star = new THREE.Mesh(geometry, material);
-
-    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
-
-    star.position.set(x, y, z);
-    scene.add(star)
-}
-
-Array(200).fill().forEach(addStar)
-
-// Infinite loop for animation and render
 function animate(){
     requestAnimationFrame(animate);
-
     torus.rotation.x += 0.01;
     torus.rotation.y += 0.005;
     torus.rotation.z += 0.01;
 
-    controls.update();
-
-    renderer.render (scene, camera)
+    renderer.render(scene, camera);
 }
 
 animate()
-*/
 
-
+/*
 import "./style.css";
 import Experience from "./Experience/Experience.js";
 
 const experience = new Experience(document.querySelector(".experience-canvas"));
+*/
