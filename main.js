c1b6e6daa45d58a33d3f66ce80a86a69cd74078a
Daniel Nguyen <nguyendaniel1997@gmail.com>
2023-07-19 18:36:37 -0500

    Configed for host

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
 import Experience from "./Experience/Experience.js";
-import Navigation from "./NavigationBar";
 
 const experience = new Experience(document.querySelector(".experience-canvas"));
-const navigation = new Navigation;
